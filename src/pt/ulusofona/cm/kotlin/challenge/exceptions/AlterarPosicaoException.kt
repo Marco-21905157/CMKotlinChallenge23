5b8b6cd88b002e704dacd83999273e13e0db7280
MARCO\prote <marcoviana03@gmail.com>
2023-03-25 17:21:33 +0000

    AlteraoPosicaoExcepetion fixed

--- src/pt/ulusofona/cm/kotlin/challenge/exceptions/AlterarPosicaoException.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/exceptions/AlterarPosicaoException.kt
@@ -1,3 +1,3 @@
 package pt.ulusofona.cm.kotlin.challenge.exceptions
 
-class AlterarPosicaException(): Exception()
+class AlterarPosicaoException(): Exception()
--- src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
@@ -1,12 +1,12 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 
-import pt.ulusofona.cm.kotlin.challenge.exceptions.AlterarPosicaException
+import pt.ulusofona.cm.kotlin.challenge.exceptions.AlterarPosicaoException
 
 class Posicao(var x: Int, var y: Int) {
     fun alterarPosicaoPara(x: Int, y: Int) {
 
         if (this.x == x && this.y == y) {
-            throw AlterarPosicaException()
+            throw AlterarPosicaoException()
         }
         this.x = x
         this.y = y
